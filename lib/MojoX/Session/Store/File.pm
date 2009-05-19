13bb74c3c9da74de9fb3e0381619cde0b76c97c0
Kazuhiro Shibuya <stevenlabs@gmail.com>
2009-05-20 08:21:48 +0900

    initial commit

---
+++ lib/MojoX/Session/Store/File.pm
@@ -0,0 +1,33 @@
+package MojoX::Session::Store::File;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MojoX::Session::Store::File -
+
+=head1 SYNOPSIS
+
+  use MojoX::Session::Store::File;
+
+=head1 DESCRIPTION
+
+MojoX::Session::Store::File is
+
+=head1 AUTHOR
+
+Kazuhiro Shibuya E<lt>stevenlabs {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
