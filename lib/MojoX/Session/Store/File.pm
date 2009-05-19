9b1c1b267d2716faba8391d74cbb5e02e157869a
Kazuhiro Shibuya <stevenlabs@gmail.com>
2009-05-20 08:26:21 +0900

    Added main code.

--- lib/MojoX/Session/Store/File.pm
+++ lib/MojoX/Session/Store/File.pm
@@ -4,6 +4,68 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use base qw(MojoX::Session::Store);
+use MojoX::Session;
+use Storable;
+use File::Spec;
+use Data::Dumper;
+
+__PACKAGE__->attr(dir => (default => 0));
+
+sub create {
+    my ($self, $sid, $expires, $data) = @_;
+
+    $data->{expire} = $expires;
+    Storable::nstore $data, $self->_to_path($sid);
+}
+
+sub update {
+    my $self = shift;
+    $self->create(@_);
+}
+
+sub load {
+    my ($self, $sid) = @_;
+    if (open my $fh, '<', $self->_to_path($sid)) {
+	my $value = Storable::fd_retrieve($fh);
+	close $fh;
+	my $expire = delete $value->{expire};
+	return ($expire, $value);
+    }
+    undef;
+}
+
+sub delete {
+    my ($self, $sid) = @_;
+    unlink $self->_to_path($sid);
+}
+
+sub _to_path {
+    my ($self, $sid) = @_;
+
+    Carp::croak "Doesn't have a dir attribute." unless $self->dir;
+    Carp::croak "Doesn't exist a dir path." unless -d $self->dir;
+    File::Spec->catfile($self->dir, $sid . '.dat');
+}
+
+sub delete_expire_session_files {
+    my $class = shift;
+    my $dir = shift;
+
+    my $fh;
+    opendir($fh, $dir) or Carp::croak "Doesn't open dir : $dir.";
+    my @files = readdir $fh;
+    my $sess = MojoX::Session->new(store => $class->new(dir => $dir));
+    for (@files) {
+	my ($sid, $dat) = split /\./, $_;
+        next unless $dat eq 'dat';
+	$sess->load($sid);
+	if ($sess->is_expired) {
+	    unlink File::Spec->catfile($dir, $_);
+	}
+    }
+}
+
 1;
 __END__
 
@@ -21,7 +83,7 @@ MojoX::Session::Store::File is
 
 =head1 AUTHOR
 
-Kazuhiro Shibuya E<lt>stevenlabs {at} gmail.comE<gt>
+Kazuhiro Shibuya E<lt>stevenlabs <lt>at<gt> gmail.comE<gt>
 
 =head1 SEE ALSO
 
---
+++ t/store/file.t
@@ -0,0 +1,33 @@
+use Test::More 'no_plan';
+use File::Temp qw(tempdir);
+use File::Spec;
+use MojoX::Session;
+use MojoX::Session::Store::File;
+
+#my $tempdir = tempdir(DIR => ".", CLEANUP => 1);
+my $tempdir = "./tmp";
+
+my $session = MojoX::Session->new(
+   store => MojoX::Session::Store::File->new(dir => $tempdir),
+#   expires_delta => 3600,
+);
+
+$session->create;
+$session->data(hoge => "foo", bar => "boo");
+$session->flush;
+ok(-f File::Spec->catfile($tempdir, $session->sid . ".dat"), "session file exists");
+is($session->data('hoge'), "foo", "store data hoge");
+is($session->data('bar'), "boo", "store data var");
+
+$session->data(hoge => "uoo");
+$session->flush;
+is($session->data("hoge"), "uoo", "update");
+
+$session->clear('hoge');
+$session->flush;
+isnt($session->data('hoge'), "foo", "clear");
+
+#$session->expire;
+#$session->flush;
+#ok($session->sid, "delete");
+
